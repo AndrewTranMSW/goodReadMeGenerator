f20311ca18b5beba6c383e199dfc2fc9183f1220
Andrew Tran <andrewtranmsw@gmail.com>
2022-01-23 15:39:30 -0800

    gif added. tested functionality and is working

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# Good README Generator
+
+## Licensing
+
+![GitHub license](https://img.shields.io/badge/License-%22Apache%22-yellow.svg)
+
+## Description
+- This application is a README generator for polished and professional looking READMEs for the working professional and developer.
+
+## Table of Contents
+- [Functionality](#functionality)
+
+- [How to install](#installation)
+
+- [How to use](#usage)
+
+- [How to contribute](#contributing)
+
+- [How to test](#tests)
+
+- [License](#licensing)
+
+## Functionality
+![Image](./assets/ReadMe.gif)
+
+## How to install
+- NPM install or NPM i
+
+## How to use
+- This is used for generating a clean README.
+
+## How to contribute
+- Please contact me with the information below for contribution guidelines
+
+## Testing
+- Please contact me below with the information below for test instructions
+
+## Questions
+- Github: (https://github.com/andrewtranMSW)
+- E-mail address: andrewtranmsw@gmail.com
---
+++ index.js
@@ -0,0 +1,125 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      //Add this to the 'Title' section
+      type: 'input',
+      message: 'What is the title of your application?',
+      name: 'title',
+    },
+    {
+      //'Description' section
+      type: 'input',
+      message: 'Please type in a description for your application',
+      name: 'description',
+    },
+    {
+      //'Installation' section
+      type: 'input',
+      message: 'Please type in the installation instructions for your application',
+      name: 'installation',
+    },
+    {
+      //'Usage' section
+      type: 'input',
+      message: 'Please type in the usage information for your application',
+      name: 'usage',
+    },
+    {
+      //'Contributing' section
+      type: 'input',
+      message: 'Please type in the contribution guidelines for your application',
+      name: 'contributing',
+    },
+    {
+      //'Tests' section
+      type: 'input',
+      message: 'Please type in the test instructions for your application',
+      name: 'tests',
+    },
+    {
+      //'Licensing' section
+      type: 'checkbox',
+      message: 'What will the license be for your application?',
+      choices:["Apache", "BSD3", "BSD2", "GNU", "MIT", "Mozilla", "Common", "Eclipse"],
+      name: 'licensing',
+    },
+    {
+      type: 'input',
+      //Add to 'Questions' section with a link to profile
+      message: 'What is your GitHub username?',
+      name: 'username',
+    },
+    {
+      //Add to 'Questions' section with instructions on how to reach user w/ questions
+      type: 'input',
+      message: 'What is your e-mail address?',
+      name: 'email',
+    },
+  ]);
+};
+
+//Takes input and passing the values into README template
+const generateReadMe = ({
+  title,
+  description,
+  installation,
+  usage,
+  contributing,
+  tests,
+  licensing,
+  username,
+  email,
+}) => `
+# ${title}
+
+## Licensing
+
+![GitHub license](https://img.shields.io/badge/License-%22${licensing}%22-yellow.svg)
+
+## Description
+- ${description}
+
+## Table of Contents
+- [How to install](#installation)
+
+- [How to use](#usage)
+
+- [How to contribute](#contributing)
+
+- [How to test](#tests)
+
+- [License](#licensing)
+
+## How to install
+- ${installation}
+
+## How to use
+- ${usage}
+
+## How to contribute
+- ${contributing}
+
+## Testing
+- ${tests}
+
+## Questions
+- Github: (https://github.com/${username})
+- E-mail address: ${email}
+`
+//Function to run the questions through the README template
+const init = () => {
+  promptUser()
+  .then((response) => fs.writeFileSync('README.md', generateReadMe(response)))
+  .then(() => console.log("It's working!"))
+  .catch((err) => {
+    throw err
+  })
+};
+//Calls the init function
+init();
